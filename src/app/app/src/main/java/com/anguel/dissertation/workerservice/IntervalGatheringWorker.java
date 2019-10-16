e25b41ae754e7b9b5ddab7ac3caba49eb2cee4a5
modelorona <angel.hristozov@gmail.com>
2019-10-16 22:24:04 +0100

    worker now gets 10 most used. need to implement a filter for system/not directly used apps

--- src/app/app/src/main/java/com/anguel/dissertation/workerservice/IntervalGatheringWorker.java
+++ src/app/app/src/main/java/com/anguel/dissertation/workerservice/IntervalGatheringWorker.java
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -18,9 +17,12 @@ import com.anguel.dissertation.persistence.LogEvent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -41,6 +43,7 @@ public class IntervalGatheringWorker extends Worker {
 
         if (appList != null && appList.size() == 0) {
             Log.d("Executed", "######### NO APP FOUND ##########");
+            return Result.retry();
         }
 
         LogEvent logEvent = new LogEvent();
@@ -48,13 +51,21 @@ public class IntervalGatheringWorker extends Worker {
 
         if (Objects.requireNonNull(appList).size() > 0) {
             List<Map<String, String>> logEventData = new ArrayList<>();
-            for (UsageStats usageStats : appList) {
-                Map<String, String> appData = new HashMap<>();
 
+            SortedMap<Long, UsageStats> sortedAppsWithStats = new TreeMap<>();
+
+            for (UsageStats usageStats : appList) {
+                sortedAppsWithStats.put(usageStats.getTotalTimeInForeground(), usageStats);
+            }
+
+            Iterator<Map.Entry<Long, UsageStats>> appIterator = sortedAppsWithStats.entrySet().iterator();
+            int current = sortedAppsWithStats.size()>10?10:sortedAppsWithStats.size();  // see if it will get the top 10 or less
+            while (current-- != 0 && appIterator.hasNext()){
+                UsageStats usageStats = appIterator.next().getValue();
+                Map<String, String> appData = new HashMap<>();
                 appData.put("name", appName(usageStats.getPackageName()));
                 appData.put("lastTimeUsed", String.valueOf(usageStats.getLastTimeUsed()));
                 appData.put("totalTimeInForeground", String.valueOf(usageStats.getTotalTimeInForeground()));
-
 
 //                now this will be the interesting part. with android Q, more usage data is available, but we need to check to make sure the device actually supports it as the current min
 //                target version is 24
