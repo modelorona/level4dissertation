8727f52f8fa81769b514ae0c8fc9aee152c8df82
modelorona <angel.hristozov@gmail.com>
2019-11-13 21:01:07 +0000

    finishQuiz should work, and now to save actual response

--- src/app/app/src/main/java/com/anguel/dissertation/QuizActivity.java
+++ src/app/app/src/main/java/com/anguel/dissertation/QuizActivity.java
@@ -1,13 +1,25 @@
 package com.anguel.dissertation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private int total = 0;
+    private final int AMOUNT_OF_QUESTIONS = 20;
+    private int totalQuestionsSoFar = 0;
+    private TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,34 +27,79 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_quiz);
         setTitle(R.string.title);
 
+//        get all buttons
         Button ans1 = (Button) findViewById(R.id.ans1);
         Button ans2 = (Button) findViewById(R.id.ans2);
         Button ans3 = (Button) findViewById(R.id.ans3);
         Button ans4 = (Button) findViewById(R.id.ans4);
         Button ans5 = (Button) findViewById(R.id.ans5);
 
+//        attach handler to all buttons
         ans1.setOnClickListener(this);
         ans2.setOnClickListener(this);
         ans3.setOnClickListener(this);
         ans4.setOnClickListener(this);
         ans5.setOnClickListener(this);
 
-
+//        set initial question text
+        question = (TextView) findViewById(R.id.question);
+        question.setText(R.string.q1);
 
     }
 
     @Override
     public void onClick(View v) {
-        Log.d("ONCLICK", String.valueOf(v.getId()));
-//        switch(v.getId()) {
-//            case R.id.ans1:
-//                break;
-//
-//            case R.id.ans2:
-//                break;
-//
-//
-//        }
+        int value;
+        switch(v.getId()) {
+            case R.id.ans2:
+                value = 1;
+                break;
+            case R.id.ans3:
+                value = 2;
+                break;
+            case R.id.ans4:
+                value = 3;
+                break;
+            case R.id.ans5:
+                value = 4;
+                break;
+            case R.id.ans1:
+            default: // set a default value of 0. the total will not change
+                value = 0;
+                break;
+        }
+        total += value;
+        if (totalQuestionsSoFar++ < AMOUNT_OF_QUESTIONS-1) {
+            updateQuestion(getBaseContext(), totalQuestionsSoFar);
+        } else {
+            finishQuiz();
+        }
+    }
+
+    public void updateQuestion(Context context, int index) {
+        String resource = String.format("q%s", String.valueOf(index+1));
+        int requestedId = context.getResources().getIdentifier(resource, "string", context.getPackageName());
+        question.setText(getString(requestedId));
+    }
+
+    public void finishQuiz() {
+//        save score before doing anything else
+
+//        tell the user what has happened. give them chance to read more about the score
+        AlertDialog.Builder builder = new AlertDialog.Builder(QuizActivity.this);
+        builder.setMessage("Thank you for taking the test. Your SIAS score is " + total + ". If you would like to know more about this test, please click the left button below to learn more.")
+                .setTitle("Thank you");
+
+        builder.setPositiveButton("Finish", (dialog, which) -> finish());
+
+        builder.setNeutralButton("Learn more", (dialog, which) -> {
+            Intent learnMoreIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.sias_url)));
+            startActivity(learnMoreIntent);
+            finish();
+        });
+
+        builder.create().show();
+
     }
 
     @Override
