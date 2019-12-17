c57f833fe28f23c8070dc683ad8627e19e111489
modelorona <angel.hristozov@gmail.com>
2019-12-17 20:57:51 +0000

    removed methods not being used

--- src/app/app/src/main/java/com/anguel/dissertation/MainActivity.java
+++ src/app/app/src/main/java/com/anguel/dissertation/MainActivity.java
@@ -37,7 +37,7 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_CODE = 101;
+//    public static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,16 +58,17 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
                 alert.setTitle("Please enable permissions")
                         .setMessage("Please make sure to enable the Usage Statistics permisssion and then disable battery optimisations for the app to work as intended. You can find this " +
-                                "in the settings.")
-                        .setCancelable(false);
+                                "in the settings.");
+//                        .setCancelable(false);
 
                 alert.setPositiveButton("Go to Settings", ((dialog, which) -> {
-                    Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                    Intent settingsIntent = new Intent(getBaseContext(), SettingsActivity.class);
                     startActivity(settingsIntent);
                 }));
 
+
                 Dialog d = alert.create();
-                d.setCanceledOnTouchOutside(false);
+//                d.setCanceledOnTouchOutside(false);
                 d.show();
 
             } else {
@@ -78,63 +79,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void setUpBackgroundService() {
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        intent.putExtra("startTime", -1L);
-        intent.putExtra("endTime", -1L);
-
-        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-//        https://stackoverflow.com/a/25120314/4004697
-
-        long midnight;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            midnight = LocalDateTime.now().until(LocalDate.now().plusDays(1).atStartOfDay(), ChronoUnit.MILLIS);
-        } else { // use backported version only when necessary
-            Log.d("BP", "using backported version");
-            midnight = org.threeten.bp.LocalDateTime.now().until(org.threeten.bp.LocalDate.now().plusDays(1).atStartOfDay(), org.threeten.bp.temporal.ChronoUnit.MILLIS);
-        }
-        midnight += System.currentTimeMillis();
-        Log.d("midnightTime", String.valueOf(System.currentTimeMillis() + midnight));
-
-//        create the background task to start at midnight and then run every 4 hours.
-        Objects.requireNonNull(alarmManager).setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 2000, AlarmManager.INTERVAL_HOUR * 4, pendingIntent);
-    }
-
-    public String getUserID() {
-//        preferencemanager was deprecated
-        SharedPreferences sharedPref = this.getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        String id = sharedPref.getString(getString(R.string.shprefprefix)+"_ID", "");
-        if (id.equalsIgnoreCase("")) {
-            UUID g = UUID.randomUUID();
-            id = g.toString();
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString(getString(R.string.shprefprefix)+"_ID", id);
-            editor.apply();
-        }
-        return id;
-    }
-
-//    @SuppressLint("BatteryLife")
-    public void disableBatteryOptimisation() {
-        startActivityForResult(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS), REQUEST_CODE+1);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-//        OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(IntervalGatheringWorker.class).build();
-//        WorkManager.getInstance(this.getApplicationContext()).enqueue(request);
-    }
-
-
-    void requestUsageStatsPermission() {
-        if (!hasUsageStatsPermission(this)) {
-            startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), REQUEST_CODE);
-        }
-    }
+//    public void setUpBackgroundService() {
+//        Intent intent = new Intent(this, AlarmReceiver.class);
+//        intent.putExtra("startTime", -1L);
+//        intent.putExtra("endTime", -1L);
+//
+//        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+////        https://stackoverflow.com/a/25120314/4004697
+//
+//        long midnight;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            midnight = LocalDateTime.now().until(LocalDate.now().plusDays(1).atStartOfDay(), ChronoUnit.MILLIS);
+//        } else { // use backported version only when necessary
+//            Log.d("BP", "using backported version");
+//            midnight = org.threeten.bp.LocalDateTime.now().until(org.threeten.bp.LocalDate.now().plusDays(1).atStartOfDay(), org.threeten.bp.temporal.ChronoUnit.MILLIS);
+//        }
+//        midnight += System.currentTimeMillis();
+//        Log.d("midnightTime", String.valueOf(System.currentTimeMillis() + midnight));
+//
+////        create the background task to start at midnight and then run every 4 hours.
+//        Objects.requireNonNull(alarmManager).setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 2000, AlarmManager.INTERVAL_HOUR * 4, pendingIntent);
+//    }
+//
+//    public String getUserID() {
+////        preferencemanager was deprecated
+//        SharedPreferences sharedPref = this.getSharedPreferences(
+//                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        String id = sharedPref.getString(getString(R.string.shprefprefix)+"_ID", "");
+//        if (id.equalsIgnoreCase("")) {
+//            UUID g = UUID.randomUUID();
+//            id = g.toString();
+//            SharedPreferences.Editor editor = sharedPref.edit();
+//            editor.putString(getString(R.string.shprefprefix)+"_ID", id);
+//            editor.apply();
+//        }
+//        return id;
+//    }
+//
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     boolean hasUsageStatsPermission(Context context) {
@@ -144,16 +127,16 @@ public class MainActivity extends AppCompatActivity {
         return mode == AppOpsManager.MODE_ALLOWED;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            disableBatteryOptimisation();
-        } else if (requestCode == REQUEST_CODE+1) {
-            setUpBackgroundService();
-        }
-
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_CODE) {
+//            disableBatteryOptimisation();
+//        } else if (requestCode == REQUEST_CODE+1) {
+//            setUpBackgroundService();
+//        }
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
