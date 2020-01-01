3e74a3da019a1971a286dc788cb2a479d60999c8
Anguel <angel.hristozov@gmail.com>
2020-01-01 20:58:18 +0100

    added data collection blank activity and split toolbar into own xml

---
+++ src/app/app/src/main/java/com/anguel/dissertation/datacollection/DataCollectionActivity.java
@@ -0,0 +1,17 @@
+package com.anguel.dissertation.datacollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anguel.dissertation.R;
+
+public class DataCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+        setTitle("Data Collection");
+    }
+}
