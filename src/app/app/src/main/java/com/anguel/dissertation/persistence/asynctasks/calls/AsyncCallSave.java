9a9455ecb362df2de73de0f6508eb160a11ab821
modelorona <angel.hristozov@gmail.com>
2020-01-30 21:19:38 +0000

    moved call tasks outside

--- src/app/app/src/main/java/com/anguel/dissertation/persistence/asynctasks/calls/AsyncCallSave.java
+++ src/app/app/src/main/java/com/anguel/dissertation/persistence/asynctasks/calls/AsyncCallSave.java
@@ -1,11 +1,11 @@
-package com.anguel.dissertation.persistence.logger.asynctasks.calls;
+package com.anguel.dissertation.persistence.asynctasks.calls;
 
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.anguel.dissertation.persistence.database.calls.Call;
-import com.anguel.dissertation.persistence.database.calls.CallDatabase;
+import com.anguel.dissertation.persistence.entity.calls.Call;
+import com.anguel.dissertation.persistence.entity.calls.CallDatabase;
 
 public class AsyncCallSave extends AsyncTask<Object, Void, Boolean> {
     @Override
